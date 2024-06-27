c7548385850f4f83d08a8b75afeb4d61a02e4a20
Luis Felipe <luis.hernandez@derivationfund.onmicrosoft.com>
2024-06-27 15:51:30 -0500

    prueba de contexto 2

--- main.tf
+++ main.tf
@@ -26,6 +26,20 @@ resource "aws_instance" "test_medium" {
   }
 }
 
+resource "aws_instance_small" "test_medium" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  user_data     = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y python3 python3-pip nodejs npm golang
+    EOF
+
+  tags = {
+    Name = "test-small"
+  }
+}
+
 
 
 
